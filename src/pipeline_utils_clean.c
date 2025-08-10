cd75cb28b2e288465ba8284c5e4fa374cba7fc29
Mourtada Bouizakarne <mbouizak@c1r3p5.1337.ma>
2025-08-10 18:02:43 +0100

    is clooooose

--- src/pipeline_utils_clean.c
+++ src/pipeline_utils_clean.c
@@ -6,7 +6,7 @@
 /*   By: mbouizak <mbouizak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 22:00:00 by mbouizak          #+#    #+#             */
-/*   Updated: 2025/08/10 14:51:13 by mbouizak         ###   ########.fr       */
+/*   Updated: 2025/08/10 18:00:22 by mbouizak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,9 @@ static void	handle_child_fork(t_child_params *child_params,
 		child_params->env);
 }
 
-int	process_pipeline_iteration(t_child_params *child_params,
-		t_exec_params *params, t_parent_params *parent_params)
+int	process_pipeline_helper(int pid, t_parent_params *parent_params,
+		t_child_params *child_params, t_exec_params *params)
 {
-	pid_t	pid;
-
-	params->prev_fd = *(parent_params->prev_fd);
-	params->save = dup(0);
-	if (params->save < 0)
-		return (0);
-	if (!setup_pipe(child_params->curr, params->fd))
-	{
-		close(params->save);
-		dup2(params->saved_stdin, 0);
-		close(params->saved_stdin);
-		return (0);
-	}
-	pid = fork();
 	if (pid == 0)
 		handle_child_fork(child_params, params);
 	else if (pid > 0)
@@ -76,6 +62,47 @@ int	process_pipeline_iteration(t_child_params *child_params,
 	return (1);
 }
 
+int	process_pipeline_iteration(t_child_params *child_params,
+		t_exec_params *params, t_parent_params *parent_params)
+{
+	pid_t	pid;
+
+	params->prev_fd = *(parent_params->prev_fd);
+	params->save = dup(0);
+	if (params->save < 0)
+		return (0);
+	if (!setup_pipe(child_params->curr, params->fd))
+	{
+		close(params->save);
+		dup2(params->saved_stdin, 0);
+		close(params->saved_stdin);
+		return (0);
+	}
+	pid = fork();
+	if (process_pipeline_helper(pid, parent_params, child_params, params) == 0)
+		return (0);
+	// if (pid == 0)
+	// 	handle_child_fork(child_params, params);
+	// else if (pid > 0)
+	// 	handle_parent_process(pid, child_params->curr, parent_params);
+	// else
+	// {
+	// 	handle_fork_error(params->save, params->saved_stdin);
+	// 	return (0);
+	// }
+	// if (params->save >= 0)
+	// {
+	// 	if (!child_params->curr->next)
+	// 	{
+	// 		close(parent_params->fd[0]);
+	// 		close(parent_params->fd[1]);
+	// 	}
+	// 	dup2(params->save, 0);
+	// 	close(params->save);
+	// }
+	return (1);
+}
+
 static void	cleanup_pipeline_fds(t_exec_params *params, t_pipeline_state *state)
 {
 	if (state->fd_in > 2)
@@ -90,6 +117,38 @@ static void	cleanup_pipeline_fds(t_exec_params *params, t_pipeline_state *state)
 		close(state->prev_fd);
 }
 
+int setup_params_helper(t_child_params *child_params, t_exec_params *params, t_pipeline_state state, t_parent_params parent_params)
+{
+	t_command *curr;
+
+	curr = child_params->cmd;
+	while (curr && !(child_params->cmd)->file)
+	{
+		child_params->curr = curr;
+		if (!process_pipeline_iteration(child_params, params, &parent_params))
+		{
+			cleanup_pipeline_fds(params, &state);
+			dup2(params->saved_stdin, 0);
+			close(params->saved_stdin);
+			return (0);
+		}
+		curr = curr->next;
+	}
+	return (1);
+}
+
+// void init_fds(t_exec_params *params, t_parent_params *parent_params, t_pipeline_state *state)
+// {
+// 	params->fd[0] = 0;
+// 	params->fd[1] = 0;
+// 	parent_params->prev_fd = &state->prev_fd;
+// 	parent_params->fd = params->fd;
+// 	parent_params->pids = (pid_t[1024]){0};
+// 	parent_params->pid_count = &state->pid_count;
+// 	//child_params->cmd = cmd;
+// 	//child_params->data = data;
+// }
+
 static void	setup_params_and_loop(t_command *cmd, t_data **data, char ***env,
 		t_exec_params *params)
 {
@@ -99,6 +158,7 @@ static void	setup_params_and_loop(t_command *cmd, t_data **data, char ***env,
 	t_command			*curr;
 
 	initialize_pipeline_state(params, &state);
+	//init_fds(params, &parent_params, &state);
 	params->fd = (int[2]){0, 0};
 	parent_params.prev_fd = &state.prev_fd;
 	parent_params.fd = params->fd;
@@ -108,18 +168,20 @@ static void	setup_params_and_loop(t_command *cmd, t_data **data, char ***env,
 	child_params.data = data;
 	child_params.env = env;
 	curr = cmd;
-	while (curr && !(cmd)->file)
-	{
-		child_params.curr = curr;
-		if (!process_pipeline_iteration(&child_params, params, &parent_params))
-		{
-			cleanup_pipeline_fds(params, &state);
-			dup2(params->saved_stdin, 0);
-			close(params->saved_stdin);
-			return ;
-		}
-		curr = curr->next;
-	}
+	if (!setup_params_helper(&child_params, params, state, parent_params))
+		return ;
+	// while (curr && !(cmd)->file)
+	// {
+	// 	child_params.curr = curr;
+	// 	if (!process_pipeline_iteration(&child_params, params, &parent_params))
+	// 	{
+	// 		cleanup_pipeline_fds(params, &state);
+	// 		dup2(params->saved_stdin, 0);
+	// 		close(params->saved_stdin);
+	// 		return ;
+	// 	}
+	// 	curr = curr->next;
+	// }
 	cleanup_pipeline_fds(params, &state);
 	handle_core_dumped(parent_params.pids, state.pid_count, data);
 	signal(SIGINT, my_handler);
