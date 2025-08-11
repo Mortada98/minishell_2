a29260642ec2a3446a67a2297aedc89950be88ff
Hicham Elfatihi <helfatih@c1r4p4.1337.ma>
2025-08-11 15:29:48 +0100

    fix expansion

--- src/Handle_token.c
+++ src/Handle_token.c
@@ -6,7 +6,7 @@
 /*   By: helfatih <helfatih@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 15:10:54 by mbouizak          #+#    #+#             */
-/*   Updated: 2025/08/11 13:17:31 by helfatih         ###   ########.fr       */
+/*   Updated: 2025/08/11 15:26:04 by helfatih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 void	lexe_with_space(t_token **token, int *start, int *i, char *word)
 {
 	char	*str;
+	bool	join;
 
+	join = false;
+	if (word[*i] == 0)
+		join = true;
 	if (*i > *start)
 	{
 		str = gc_substr(word, *start, *i - *start);
-		add_token(token, creat_token(str, TOKEN_WORD, false, false));
+		add_token(token, creat_token(str, TOKEN_WORD, join, false));
 	}
 	while ((word[*i] == ' ' || word[*i] == '\t'))
 		(*i)++;
